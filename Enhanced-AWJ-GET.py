205d3c1e36492b322f9f84964d2003e4a713c175
Alberto Righetto <arighetto88@gmail.com>
2026-01-28 19:14:09 +0100

    Version bump

--- Enhanced-AWJ-GET.py
+++ Enhanced-AWJ-GET.py
@@ -11,7 +11,7 @@ TCP_PORT = 10606
 class TCPGuiApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("Enhanced AWJ-GET - v1.1.1")
+        self.root.title("Enhanced AWJ-GET - v1.1")
         self.root.geometry("1300x560")
         self.root.configure(bg="#393939")
         # Base64 encoded image data
@@ -86,7 +86,7 @@ class TCPGuiApp:
             "Source code on GitHub at\n" \
             "https://github.com/albertorighetto/enhanced-awj-get\n" \
             "\n" \
-            "v1.1.1\n" \
+            "v1.1\n" \
             "\n" \
             "This software is not affiliated with,\n" \
             "endorsed by, or supported by Analog Way",
